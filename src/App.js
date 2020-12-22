db128c0c5595659005a6191571b0e4c832818aa9
adelinel <adeline.latruwe@gmail.com>
2020-12-22 13:29:58 -0800

    add API request

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from "react";
 import "./App.css";
-import BookList from "./composition/Book";
+import BookList from "./composition/BookList.js";
 import NavBar from "./composition/NavBar.js";
 import SearchArea from "./composition/SearchArea.js";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: "henry",
-      bookListResults: {},
-    };
-  }
   render() {
     return (
       <main className="App">
         <NavBar />
         <SearchArea />
-        <BookList
-          searchTerm={this.state.searchTerm}
-          bookListResults={this.state.bookListResults}
-        />
+        <BookList />
       </main>
     );
   }
--- src/composition/BookList.js
+++ src/composition/BookList.js
@@ -2,13 +2,38 @@ import React from "react";
 import Book from "./Book.js";
 
 class BookList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "henry",
+      bookListResults: [],
+    };
+  }
+
+  componentDidMount() {
+    const baseUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+    const apiKey = "AIzaSyCtT-5Hl4UsRhciJF09qvNCsKMmC4gDfvQ";
+    const searchTerm = this.state.searchTerm;
+    const url = baseUrl + searchTerm + "&" + apiKey;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const volumes = data.items;
+        const volumesItem = Object.keys(volumes).map(
+          (key) => volumes[key].selfLink
+        );
+        console.log(data);
+        console.log(volumes);
+        console.log(volumesItem);
+        this.setState({
+          bookListResults: volumesItem,
+        });
+      });
+  }
+
   render() {
-    return (
-      <div>
-        <Book />
-        <Book />
-      </div>
-    );
+    return <Book bookListResults={this.state.bookListResults} />;
   }
 }
 
